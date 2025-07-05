a64cdfaa0d11c8d7f4f09f4dffed18fa2f2fa55c
1eehvunzin <hyunjinlee@ewha.ac.kr>
2025-07-05 12:30:28 +0900

    사이드 바 작성 중

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,7 @@
+import moreIcon from '../assets/image/more.png';
+import addIcon from '../assets/image/add.png';
+import hideIcon from '../assets/image/hide.png';
+
 export default function Sidebar({ isOpen, onClose }) {
     return (
       <aside style={{
@@ -13,13 +17,35 @@ export default function Sidebar({ isOpen, onClose }) {
         zIndex: 1,
       }}>
         <h1>List</h1>
-        <nav>
-          <ul>
-            <li>메뉴 1</li>
-            <li>메뉴 2</li>
-          </ul>
-        </nav>
-        <button>Add List</button>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: "space-between"
+        }}>
+          <h4 style={{opacity: 0.5}}>대학로 분좋카</h4>
+          <div>
+            <button>add</button>
+            
+
+            <button
+              onClick={onMenuClick}
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                border: 'none',
+                padding: '0',
+                cursor: 'pointer'}}>
+                <img src={moreIcon} alt='menu icon'></img>
+            </button>
+          </div>
+        </div>
+        <li>메뉴 1</li>
+        <li>메뉴 2</li>
+          
+        <div style={{ display: 'flex', gap: '8px' }}>
+            <button>Add List</button>
+            <button>Add Course</button>
+        </div>
       </aside>
     );
   }
