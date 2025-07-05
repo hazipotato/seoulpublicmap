6b514f62ef2b3832843ac8fd55e2212d7f0553ea
@wowpc <hunjin0208@naver.com>
2025-07-05 14:45:44 +0900

    스테이지에서 하위 폴더 삭제

---
+++ seoulpublicmap/src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import menuIcon from '../assets/image/menu-icon.png';
+
+function Header({ onMenuClick}) {
+  return (
+    <header style={{
+      display: 'flex',
+      alignItems: 'center', //세로 정렬
+      padding: '1rem 2rem',
+      backgroundColor: '#ffffff',
+      color: 'black',
+      boxShadow: '0 4px 4px rgba(0, 0, 0, 0.25)',
+      position: 'relative',
+      zIndex: 2,
+    }}>
+      {/* 왼쪽 - 메뉴 버튼 (해당 형식은 JSX 주석임)*/}
+      <button
+        onClick={onMenuClick}
+        style={{
+          flex: 1,
+          backgroundColor: 'transparent',
+          border: 'none',
+          padding: '0',
+          textAlign: 'left',
+          cursor: 'pointer'}}>
+          <img src={menuIcon} alt='menu icon'></img>
+        </button>
+
+      {/* 가운데 - 제목 */}
+      <h1 style={{ flex: 1, textAlign: 'center', margin: 0 }}>SEOUL PUBLIC MAP</h1>
+
+      {/* 오른쪽 - 비어 있는 공간 (좌우 균형용) */}
+      <div style={{ flex: 1 }} />
+    </header>
+  );
+}
+
+export default Header;
---
+++ seoulpublicmap/src/components/Sidebar.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import moreIcon from '../assets/image/more.png';
+import addIcon from '../assets/image/add.png';
+import hideIcon from '../assets/image/hide.png';
+
+import '../assets/style/style.css';
+
+export default function Sidebar({ isOpen, onClose }) {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const onBtnClick = () => {
+    console.log('Button clicked');
+  };
+
+  const onMoreClick = () => {
+    setIsToggled(prev => !prev); // true <-> false 토글
+  };
+
+    return (
+      <aside style={{
+        width: isOpen ? '22.22vw' /* 또는 '22.22%' (뷰포트 기준 너비)' */ : '0',
+        transition: 'width 0.3s',
+        overflow: 'hidden',
+        backgroundColor: '#ffffff',
+        color: 'black',
+        height: '100vh',
+        padding: isOpen ? '1rem' : '0',
+        boxShadow: '0 4px 4px rgba(0, 0, 0, 0.25)',
+        position: 'absolute',
+        zIndex: 1,
+      }}>
+        <h1 style={{marginBottom: 0}}>List</h1>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <input type="text" placeholder=" 검색창 (임시) " />
+        </div>
+        
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: "space-between"
+        }}>
+          <h4 style={{opacity: 0.5, margin: '0'}}>대학로 분좋카</h4>
+          <div>
+            <button 
+              onClick={onBtnClick}
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                border: 'none',
+                cursor: 'pointer'}}>
+                <img src={addIcon} alt='add icon'></img>
+            </button>
+            <button
+              onClick={onMoreClick}
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                border: 'none',
+                cursor: 'pointer'}}>
+                <img src={isToggled ? moreIcon : hideIcon} alt="more icon" />
+            </button>
+          </div>
+        </div>
+        <ul>
+          <li>청킹</li>
+          <li>그루</li>
+          <li>타르트앤</li>
+          <li>커피한약방</li>
+        </ul>
+        
+          
+        <div style={{ display: 'flex', gap: '8px' }}>
+            <button>Add List</button>
+            <button>Add Course</button>
+        </div>
+      </aside>
+    );
+  }
+  
